c4cffacce44e39bd36c51bc977ace7736b2d273a
rezamaddahi72 <rezamaddahi72@gmail.com>
2025-03-23 07:31:11 +0330

    define components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from './header/header.component';
+import {UserComponent} from "./user/user.component";
+import {DUMMY_USERS} from './dummy-users';
+import {NgFor} from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+    imports: [RouterOutlet, HeaderComponent, UserComponent, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'users';
+
+  users = DUMMY_USERS;
 }
