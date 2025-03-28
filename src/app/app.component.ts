b63f345c3500af9ab60e7de96ee0213c3dc1b582
rezamaddahi72 <rezamaddahi72@gmail.com>
2025-03-28 07:47:15 +0330

    add loop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,24 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {HeaderComponent} from './header/header.component';
-import {UserComponent} from "./user/user.component";
-import {DUMMY_USERS} from './dummy-users';
-import {CommonModule, NgFor} from '@angular/common';
-import {TasksComponent} from './tasks/tasks.component';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from "./user/user.component";
+import { DUMMY_USERS } from './dummy-users';
+import { NgFor, NgIf } from '@angular/common';
+import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, UserComponent, NgFor, CommonModule, TasksComponent],
+  imports: [ HeaderComponent, UserComponent, NgFor, TasksComponent, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
 
   users = DUMMY_USERS;
-  selectedUserId = 'u1';
+  selectedUserId = '';
 
   get selectedUser() {
-    return this.users.find((user) => user.id === this.selectedUserId)!;
+    return this.users.find((user) => user.id === this.selectedUserId);
   }
 
   onSelectUser(id: string) {
