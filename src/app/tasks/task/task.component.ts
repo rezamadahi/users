5b6985aa6b2a854d20f9a71a8dfad2e2fc523059
rezamaddahi72 <rezamaddahi72@gmail.com>
2025-03-28 08:44:36 +0330

    add task component

---
+++ src/app/tasks/task/task.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+import { Task } from '../task.model';
+import { DatePipe } from '@angular/common';
+
+@Component({
+  selector: 'app-task',
+  standalone: true,
+  imports: [DatePipe],
+  templateUrl: './task.component.html',
+  styleUrl: './task.component.css'
+})
+export class TaskComponent {
+
+  @Input() task!: Task;
+
+}
