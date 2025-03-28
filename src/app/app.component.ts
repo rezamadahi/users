a760e277cc85282d9432afd0a38b201a2f29f3ca
rezamaddahi72 <rezamaddahi72@gmail.com>
2025-03-28 04:16:22 +0330

    add task component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,26 @@ import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from './header/header.component';
 import {UserComponent} from "./user/user.component";
 import {DUMMY_USERS} from './dummy-users';
-import {NgFor} from '@angular/common';
+import {CommonModule, NgFor} from '@angular/common';
+import {TasksComponent} from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-    imports: [RouterOutlet, HeaderComponent, UserComponent, NgFor],
+  imports: [RouterOutlet, HeaderComponent, UserComponent, NgFor, CommonModule, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
 
   users = DUMMY_USERS;
+  selectedUserId = 'u1';
+
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId)!;
+  }
+
+  onSelectUser(id: string) {
+    this.selectedUserId = id;
+  }
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,20 @@
-import { Component } from '@angular/core';
-import {DUMMY_USERS} from '../dummy-users';
+// import {Component, computed, Input, signal} from '@angular/core';
+// import { DUMMY_USERS } from '../dummy-users';
+// const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+import {Component, computed, EventEmitter, Input, input, Output} from '@angular/core';
+
+// type User = {
+//   id: string,
+//   name: string,
+//   avatar: string,
+// };
+
+interface User {
+  id: string;
+  name: string;
+  avatar: string;
+}
 
 @Component({
   selector: 'app-user',
@@ -11,15 +24,37 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex];
+  // selectedUser = signal(DUMMY_USERS[randomIndex]);
+  // imagePath = computed(() => 'assets/images/users/' + this.selectedUser().avatar);
+
+  // get imagePath() {
+  //   return 'assets/images/users/' + this.selectedUser().avatar;
+  // }
+
+  // onSelectUser() {
+  //   const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+  //   this.selectedUser.set(DUMMY_USERS[randomIndex]);
+  // }
+
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+  //
+  // imagePath = computed(() => {
+  //   return 'assets/images/users/' + this.avatar();
+  // });
+
+  // @Input({required: true}) id!: string;
+  // @Input({required: true}) avatar!: string;
+  // @Input({required: true}) name!: string;
+  @Input({required: true}) user!: User;
+  @Output() selectUser = new EventEmitter<string>();
 
   get imagePath() {
-    return 'assets/images/users/' + this.selectedUser.avatar;
+    return 'assets/images/users/' + this.user.avatar;
   }
 
   onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser = DUMMY_USERS[randomIndex];
+    this.selectUser.emit(this.user.id);
   }
 
 }
